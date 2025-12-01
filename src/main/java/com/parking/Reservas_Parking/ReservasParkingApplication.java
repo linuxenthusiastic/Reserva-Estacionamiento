74547725c0d9016102ddb25cec96aae6a23b6d86
zodiacsilence <mbitakrec@gmail.com>
2025-12-01 12:56:15 -0400

    chore: base inicial Spring Boot generate for Spring Initializr

---
+++ src/main/java/com/parking/Reservas_Parking/ReservasParkingApplication.java
@@ -0,0 +1,13 @@
+package com.parking.Reservas_Parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasParkingApplication.class, args);
+	}
+
+}
