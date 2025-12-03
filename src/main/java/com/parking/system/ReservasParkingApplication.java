13c9ec07de615fd9ffaa8d6f1e6967586c3f4af8
zodiacsilence <mbitakrec@gmail.com>
2025-12-03 15:08:40 -0400

    chore: error en las rutas de los archivos

--- src/main/java/com/parking/system/ReservasParkingApplication.java
+++ src/main/java/com/parking/system/ReservasParkingApplication.java
@@ -1,4 +1,4 @@
-package com.parking.Reservas_Parking;
+package com.parking.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
